558b8bb557a34302a1f116488d9cce09e91c6787
root <root@localhost.localdomain>
2024-02-29 21:31:20 +0100

    Adding IGW,NAT,Routing groups

--- main.tf
+++ main.tf
@@ -38,6 +38,66 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.backend_subnets[1].id
+
+  tags = {
+    Name = "NAT"
+  }
+}
+
+resource "aws_route_table" "frontend_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Frontend Route Table"
+  }
+}
+
+resource "aws_route_table" "backend_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Backend Route Table"
+  }
+}
+
+resource "aws_route_table" "database_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    blackhole = true
+  }
+
+  tags = {
+    Name = "Database Route Table"
+  }
+}
+
 resource "aws_subnet" "frontend_subnets" {
   count             = length(var.frontend_subnet_cidr)
   vpc_id            = aws_vpc.main.id
